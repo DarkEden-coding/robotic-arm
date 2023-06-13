5e458c52387f5810cb335d26350988a8594ac77e
DarkEden-coding <darkedenc9@gmail.com>
2023-06-13 16:51:35 -0400

    f

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
+from time import sleep
 
 direction = 14  # Direction (DIR) GPIO Pin
 step = 15  # Step GPIO Pin
@@ -7,22 +8,22 @@ step = 15  # Step GPIO Pin
 # Declare a instance of class pass GPIO pins numbers and the motor type
 mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
 
-mymotortest.motor_go(
-    False,  # True=Clockwise, False=Counter-Clockwise
-    "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
-    50,  # number of steps
-    0.0005,  # step delay [sec]
-    False,  # True = print verbose output
-    0.05,
-)  # initial delay [sec]
+while True:
+    mymotortest.motor_go(
+        False,  # True=Clockwise, False=Counter-Clockwise
+        "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+        50,  # number of steps
+        0.0005,  # step delay [sec]
+        False,  # True = print verbose output
+        0.05,
+    )  # initial delay [sec]
 
-mymotortest.motor_go(
-    True,  # True=Clockwise, False=Counter-Clockwise
-    "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
-    50,  # number of steps
-    0.0005,  # step delay [sec]
-    False,  # True = print verbose output
-    0.05,
-)  # initial delay [sec]
-
-GPIO.cleanup()  # clear GPIO allocations after run
+    mymotortest.motor_go(
+        True,  # True=Clockwise, False=Counter-Clockwise
+        "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
+        50,  # number of steps
+        0.0005,  # step delay [sec]
+        False,  # True = print verbose output
+        0.05,
+    )  # initial delay [sec]
+    sleep(1)

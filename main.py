ff64d73a55bb52cb235d89c7a47cdf589b165f56
DarkEden-coding <darkedenc9@gmail.com>
2023-06-13 16:47:17 -0400

    f

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import RPi.GPIO as GPIO
 from RpiMotorLib import RpiMotorLib
-import time
 
-direction = 22  # Direction (DIR) GPIO Pin
-step = 23  # Step GPIO Pin
-EN_pin = 24  # enable pin (LOW to enable)
+direction = 14  # Direction (DIR) GPIO Pin
+step = 15  # Step GPIO Pin
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
 mymotortest = RpiMotorLib.A4988Nema(direction, step, (21, 21, 21), "DRV8825")
-GPIO.setup(EN_pin, GPIO.OUT)  # set enable pin as output
 
-GPIO.output(EN_pin, GPIO.LOW)  # pull enable to low to enable motor
 mymotortest.motor_go(
     False,  # True=Clockwise, False=Counter-Clockwise
     "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
@@ -28,7 +24,5 @@ mymotortest.motor_go(
     False,  # True = print verbose output
     0.05,
 )  # initial delay [sec]
-
-GPIO.output(EN_pin, GPIO.HIGH)  # pull enable to low to enable motor
 
 GPIO.cleanup()  # clear GPIO allocations after run

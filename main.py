b07d107bbd89885c98fb2169f899ce0dd827c2f3
DarkEden-coding <darkedenc9@gmail.com>
2023-06-14 15:46:37 -0400

    f

--- main.py
+++ main.py
@@ -15,51 +15,44 @@ CLOCKWISE = GPIO.HIGH
 COUNTERCLOCKWISE = GPIO.LOW
 
 
-def rotate_stepper_motor(angle, direction, acceleration):
-    steps_per_revolution = 200  # Number of steps per full revolution
-    total_steps = int(
-        angle / 360 * steps_per_revolution
-    )  # Calculate total steps based on desired angle
+class StepperMotor:
+    def __init__(self, direction_pin, step_pin):
+        self.direction_pin = direction_pin
+        self.step_pin = step_pin
+        self.steps = 0
+        self.clockwise = True
+        self.acceleration = 0
+        self.step_speed = 1
 
-    # Set the motor direction
-    GPIO.output(direction_pin, direction)
+    def move(self, steps, clockwise, acceleration):
+        if clockwise:
+            GPIO.output(self.direction_pin, CLOCKWISE)
+        else:
+            GPIO.output(self.direction_pin, COUNTERCLOCKWISE)
+        self.steps = steps
+        self.clockwise = clockwize
+        self.acceleration = acceleration
 
-    # Calculate acceleration and deceleration parameters
-    acceleration_steps = int(
-        total_steps / 2
-    )  # Number of steps for acceleration and deceleration
-    acceleration = abs(acceleration)  # Ensure positive acceleration value
-
-    # Calculate the delay at maximum speed
-    max_speed_delay = (1 / (acceleration * acceleration_steps)) ** 0.5
-
-    # Generate step pulses to rotate the motor
-    current_delay = max_speed_delay
-    for step in range(total_steps):
-        GPIO.output(step_pin, GPIO.HIGH)
-        sleep(current_delay)
-        GPIO.output(step_pin, GPIO.LOW)
-        sleep(current_delay)
-
-        # Adjust the delay for acceleration and deceleration
-        if step < acceleration_steps:
-            current_delay -= (2 * acceleration * current_delay) ** 0.5
-        elif step >= total_steps - acceleration_steps:
-            current_delay += (2 * acceleration * current_delay) ** 0.5
-
-        # Ensure the delay does not go below the maximum speed delay
-        current_delay = max(current_delay, max_speed_delay)
+        for step in self.steps:
+            if step < 50:
+                self.step_speed -= self.acceleration
+            elif step > self.steps - 50:
+                self.step_speed += self.acceleration
+            GPIO.output(self.step_pin, GPIO.HIGH)
+            sleep(self.step_speed)
+            GPIO.output(self.step_pin, GPIO.LOW)
+            sleep(self.step_speed)
 
 
 # Repeat the process 10 times
 for _ in range(10):
     # Rotate 360 degrees counterclockwise with acceleration of 1000 steps per second squared
     print("Rotating 360 degrees counterclockwise")
-    rotate_stepper_motor(360, COUNTERCLOCKWISE, 1)
+    rotate_stepper_motor(steps=200, clockwise=False, acceleration=1)
     sleep(1)  # Wait for 1 second between rotations
     # Rotate 360 degrees clockwise with acceleration of -1000 steps per second squared (deceleration)
     print("Rotating 360 degrees clockwise")
-    rotate_stepper_motor(360, CLOCKWISE, -1)
+    rotate_stepper_motor(steps=200, clockwise=True, acceleration=1)
     sleep(1)  # Wait for 1 second between rotations
 
 # Clean up GPIO

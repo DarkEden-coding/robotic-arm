38c5c83a7b849d1de73a8b525797153eb5e7ed9b
DarkEden-coding <darkedenc9@gmail.com>
2023-06-13 20:25:17 -0400

    f

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-from RpiMotorLib import RpiMotorLib
-from time import sleep
+#######################################
+# Copyright (c) 2021 Maker Portal LLC
+# Author: Joshua Hrisko
+#######################################
+#
+# NEMA 17 (17HS4023) Raspberry Pi Tests
+# --- rotating the NEMA 17 clockwise
+# --- and counterclockwise in a loop
+#
+#
+#######################################
+#
 import RPi.GPIO as GPIO
+from RpiMotorLib import RpiMotorLib
+import time
 
-GPIO.setmode(GPIO.BCM)
-
-direction = 14  # Direction (DIR) GPIO Pin
-step = 15  # Step GPIO Pin
+################################
+# RPi and Motor Pre-allocations
+################################
+#
+# define GPIO pins
+direction = 22  # Direction (DIR) GPIO Pin
+step = 23  # Step GPIO Pin
 
 # Declare a instance of class pass GPIO pins numbers and the motor type
-mymotortest = RpiMotorLib.A4988Nema(direction, step, (-1, -1, -1), "DRV8825")
+mymotortest = RpiMotorLib.A4988Nema(direction, step, (-1, -1, -1), "A4988")
 
-while True:
-    print("Counter-Clockwise Test")
+dir_array = [False, True]
+for ii in range(10):
     mymotortest.motor_go(
-        False,  # True=Clockwise, False=Counter-Clockwise
+        dir_array[ii % 2],  # False=Clockwise, True=Counterclockwise
         "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
-        50,  # number of steps
-        0.05,  # step delay [sec]
+        200,  # number of steps
+        0.0005,  # step delay [sec]
         False,  # True = print verbose output
         0.05,
     )  # initial delay [sec]
-
-    print("Clockwise Test")
-    mymotortest.motor_go(
-        False,  # True=Clockwise, False=Counter-Clockwise
-        "Full",  # Step type (Full,Half,1/4,1/8,1/16,1/32)
-        50,  # number of steps
-        0.05,  # step delay [sec]
-        False,  # True = print verbose output
-        0.05,
-    )  # initial delay [sec]
-    sleep(1)
+    time.sleep(1)

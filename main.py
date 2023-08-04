0657c82736ab36f54ec7ab18493b8a46a685469a
DarkEden-coding <darkedenc9@gmail.com>
2023-08-04 18:21:37 -0400

    f

--- main.py
+++ main.py
@@ -10,17 +10,17 @@ enable_pin = 2  # Enable GPIO Pin
 motor = StepperMotor(
     direction_pin=direction_pin,
     step_pin=step_pin,
-    max_speed=600,
-    starting_speed=5,
-    acceleration=10,
+    max_speed=1200,
+    starting_speed=0,
+    acceleration=20,
     microstepping=5,
     ms_pins=ms_pins,
     enable_pin=enable_pin,
 )
 
 motor.enable()
-motor.move(1280, blocking=True, clockwise=True, debug=True)
+motor.move(3200, blocking=True, clockwise=True, debug=True)
 
-motor.move(1280, blocking=True, clockwise=False, debug=True)
+motor.move(3200, blocking=True, clockwise=False, debug=True)
 
 motor.disable()

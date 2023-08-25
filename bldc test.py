f0972764e0846848305dd340358cd8d8fc4a131b
DarkEden-coding <darkedenc9@gmail.com>
2023-08-24 22:34:53 -0400

    f

--- bldc test.py
+++ bldc test.py
@@ -14,16 +14,11 @@ pwm = GPIO.PWM(motor_pin, 1000)  # 1000 Hz frequency
 # Start PWM with an initial duty cycle of 0%
 pwm.start(0)
 
-try:
-    """while True:
-    for duty_cycle in range(0, 101, 5):  # Vary duty cycle from 0% to 100%
-        pwm.ChangeDutyCycle(duty_cycle)
-        print("Duty Cycle:", duty_cycle)
-        time.sleep(0.5)  # Wait for a moment before changing speed"""
-    pwm.ChangeDutyCycle(int(input("Duty Cycle: ")))
-    time.sleep(5)
+"""while True:
+for duty_cycle in range(0, 101, 5):  # Vary duty cycle from 0% to 100%
+    pwm.ChangeDutyCycle(duty_cycle)
+    print("Duty Cycle:", duty_cycle)
+    time.sleep(0.5)  # Wait for a moment before changing speed"""
+pwm.ChangeDutyCycle(int(input("Duty Cycle: ")))
+time.sleep(5)
 
-except KeyboardInterrupt:
-    # Clean up GPIO settings
-    pwm.stop()
-    GPIO.cleanup()

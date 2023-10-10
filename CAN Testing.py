b91e2c3a69b2a6536b4aaba42ac473fcfa68b740
DarkEden-coding <darkedenc9@gmail.com>
2023-10-09 20:17:04 -0400

    f

--- CAN Testing.py
+++ CAN Testing.py
@@ -43,7 +43,7 @@ for msg in bus:
     if msg.arbitration_id == message_id:
         error, state, result, traj_done = struct.unpack("<IBBB", bytes(msg.data[:7]))
         break
-print(error, state, result, traj_done)
+# print(error, state, result, traj_done)
 
 # Flush CAN RX buffer so there are no more old pending messages
 while not (bus.recv(timeout=0) is None):

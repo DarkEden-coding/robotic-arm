e5d891d6ef6ecebb49388d0a9f6c2565416232a6
DarkEden-coding <darkedenc9@gmail.com>
2023-11-04 15:38:46 -0400

    f

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ from CAN_Control.odrive_controller import odrive_controller
 from time import sleep
 
 controller_1 = odrive_controller(0)
-controller_2 = odrive_controller(1)
+# controller_2 = odrive_controller(1)
 
 controller_1.enable_motor()
-controller_2.enable_motor()
+# controller_2.enable_motor()
 
 sleep(10)
 
 controller_1.disable_motor()
-controller_2.disable_motor()
+# controller_2.disable_motor()

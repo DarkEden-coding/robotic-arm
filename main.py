698e98f33a18e66e73c2da9ca93dc9a91e877678
DarkEden-coding <darkedenc9@gmail.com>
2023-11-04 21:06:33 -0400

    f

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 from CAN_Control.odrive_controller import odrive_controller
 from CAN_Control.can_functions import shutdown
-from time import sleep
 
 controller_1 = odrive_controller(0)
-# controller_2 = odrive_controller(1)
+controller_2 = odrive_controller(1)
 
 controller_1.enable_motor()
-# controller_2.enable_motor()
+controller_2.enable_motor()
 
 controller_1.move_to_pos(50)
+controller_2.move_to_pos(50)
 
 controller_1.wait_for_move_complete()
+controller_2.wait_for_move_complete()
 
 controller_1.move_to_pos(0)
+controller_2.move_to_pos(0)
 
 controller_1.wait_for_move_complete()
+controller_2.wait_for_move_complete()
 
 controller_1.disable_motor()
-# controller_2.disable_motor()
+controller_2.disable_motor()
 
 shutdown()

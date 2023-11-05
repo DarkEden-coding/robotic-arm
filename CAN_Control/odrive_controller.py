67f664991f65f0f009a050d334e5fcb1b12173e1
DarkEden-coding <darkedenc9@gmail.com>
2023-11-04 22:36:08 -0400

    f

--- CAN_Control/odrive_controller.py
+++ CAN_Control/odrive_controller.py
@@ -141,11 +141,11 @@ class odrive_controller:
         send_bus_message(torque, "axis0.controller.input_torque", self.node_id)
 
     def wait_for_move(self):
-        print(abs(self.requested_position - self.get_encoder_pos()))
         while (
                 abs(self.requested_position - self.get_encoder_pos()) > 0.1
         ):
-            print(abs(self.requested_position - self.get_encoder_pos()))
+            print(f"Current position: {self.get_encoder_pos()}")
+            print(f"Requested position: {self.requested_position}")
             pass
         print("Move complete")
 

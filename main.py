b91b41782662db541aa58b8b32436b2293b8a91f
DarkEden-coding <darkedenc9@gmail.com>
2023-11-16 21:37:03 -0500

    f

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ controller_1 = odrive_controller(0)
 controller_2 = odrive_controller(1)
 controller_3 = odrive_controller(2)
 
-controller_1.enable_motor()
+"""controller_1.enable_motor()
 controller_2.enable_motor()
 controller_3.enable_motor()
 
@@ -78,6 +78,6 @@ controller_3.wait_for_move()
 
 controller_1.disable_motor()
 controller_2.disable_motor()
-controller_3.disable_motor()
+controller_3.disable_motor()"""
 
 shutdown()

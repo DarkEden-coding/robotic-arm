242f0ec5928533dca44aba95913c0388ba5a82d0
DarkEden-coding <darkedenc9@gmail.com>
2023-11-16 21:13:40 -0500

    f

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ controller_2.enable_motor()
 controller_2.move_to_angle(15)
 # controller_3.move_to_angle(15)
 
-sleep(1)
+controller_2.wait_for_move()
 
 # controller_1.move_to_angle(0)
 controller_2.move_to_angle(0)

4169957be5b3e0fcecc7a5130770f1ef290feaf1
DarkEden-coding <darkedenc9@gmail.com>
2023-11-16 21:43:23 -0500

    f

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-
 from CAN_Control.odrive_controller import odrive_controller
 from CAN_Control.can_functions import shutdown
-from time import sleep
 
 arm_1_length = 400
 arm_2_length = 430
@@ -58,7 +56,7 @@ controller_1 = odrive_controller(0)
 controller_2 = odrive_controller(1)
 controller_3 = odrive_controller(2)
 
-"""controller_1.enable_motor()
+controller_1.enable_motor()
 controller_2.enable_motor()
 controller_3.enable_motor()
 
@@ -78,6 +76,6 @@ controller_3.wait_for_move()
 
 controller_1.disable_motor()
 controller_2.disable_motor()
-controller_3.disable_motor()"""
+controller_3.disable_motor()
 
 shutdown()

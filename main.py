348997ff90ba187c3aa74997f0f980a4ad21d304
DarkEden-coding <darkedenc9@gmail.com>
2023-11-16 21:11:32 -0500

    f

--- main.py
+++ main.py
@@ -62,11 +62,11 @@ print(get_angles(200, 100, 10))
 
 controller_1 = odrive_controller(0)
 controller_2 = odrive_controller(1)
-controller_3 = odrive_controller(2)
+# controller_3 = odrive_controller(2)
 
 controller_1.enable_motor()
 controller_2.enable_motor()
-controller_3.enable_motor()
+# controller_3.enable_motor()
 
 # controller_1.move_to_angle(0)
 controller_2.move_to_angle(15)
@@ -80,6 +80,6 @@ controller_2.move_to_angle(0)
 
 controller_1.disable_motor()
 controller_2.disable_motor()
-controller_3.disable_motor()
+# controller_3.disable_motor()
 
 shutdown()

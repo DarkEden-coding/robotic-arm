a4bdfe109779c9b455257763cfe633719cb0b00d
DarkEden-coding <darkedenc9@gmail.com>
2023-11-18 20:26:34 -0500

    f

--- CAN_Control/odrive_controller.py
+++ CAN_Control/odrive_controller.py
@@ -74,9 +74,10 @@ def setup(node_id, gear_ratio):
     send_bus_message(None, "clear_errors", node_id)
     print("ODrive errors cleared")
 
-    send_bus_message(2 * (gear_ratio / 25), "axis0.trap_traj.config.vel_limit", node_id)
+    send_bus_message(5, "axis0.controller.config.input_mode", node_id)
 
-    send_bus_message(15, "axis0.controller.config.input_mode", node_id)
+    send_bus_message(5 * (gear_ratio / 25), "axis0.trap_traj.config.vel_limit", node_id)
+
     send_bus_message(
         0.3 * (gear_ratio / 25), "axis0.trap_traj.config.accel_limit", node_id
     )

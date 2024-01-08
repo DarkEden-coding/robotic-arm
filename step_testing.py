dc858e9824b700127e969d9f3c53197cf90283dc
DarkEden-coding <darkedenc9@gmail.com>
2024-01-07 20:57:55 -0500

    na

--- step_testing.py
+++ step_testing.py
@@ -24,18 +24,18 @@ pitch_motor = StepperMotorController(
 yaw_motor.enable_motor()
 pitch_motor.enable_motor()
 
-yaw_motor.set_micro_steps(32)
-pitch_motor.set_micro_steps(32)
+yaw_motor.set_micro_steps(64)
+pitch_motor.set_micro_steps(64)
 
 """yaw_motor.move_to_angle(90)
 pitch_motor.move_to_angle(90)
 """
 
-yaw_motor.force_move_steps(12800, .000001)
-pitch_motor.force_move_steps(6400, .000001)
+yaw_motor.force_move_steps(12800, .0001)
+pitch_motor.force_move_steps(6400, .0001)
 
-yaw_motor.force_move_steps(-12800, .000001)
-pitch_motor.force_move_steps(-6400, .000001)
+yaw_motor.force_move_steps(-12800, .0001)
+pitch_motor.force_move_steps(-6400, .0001)
 
 yaw_motor.disable_motor()
 pitch_motor.disable_motor()

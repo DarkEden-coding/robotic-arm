c9eed87f2f62fc2d55264acb8b5a286ff2dc0ca7
DarkEden-coding <darkedenc9@gmail.com>
2024-01-07 20:39:02 -0500

    na

--- step_testing.py
+++ step_testing.py
@@ -31,8 +31,11 @@ pitch_motor.set_micro_steps(64)
 pitch_motor.move_to_angle(90)
 """
 
-yaw_motor.force_move_steps(1000, .001)
-pitch_motor.force_move_steps(1000, .001)
+yaw_motor.force_move_steps(5000, .001)
+pitch_motor.force_move_steps(5000, .001)
+
+yaw_motor.force_move_steps(-5000, .001)
+pitch_motor.force_move_steps(-5000, .001)
 
 yaw_motor.disable_motor()
 pitch_motor.disable_motor()
